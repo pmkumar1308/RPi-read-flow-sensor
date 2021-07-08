b8ef171c268fb5dd30bab7e57fa0611f26930976
pmkumar1308 <pmkumar1308@gmail.com>
2021-07-08 18:23:33 +0100

    After modifying it to read and set simultaneuosly in same while loop, still the data is mixed

---
+++ scripts/Pi-set-and-read.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+import time
+from MCP4922 import MCP4922
+import sys
+import RPi.GPIO as GPIO
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs = digitalio.DigitalInOut(board.CE1)
+mcp = MCP.MCP3008(spi, cs)
+channel = AnalogIn(mcp, MCP.P0)
+
+GPIO.setmode(GPIO.BCM)   # use the Broadcom pin numbering
+GPIO.setup(17,  GPIO.OUT)
+GPIO.setwarnings(False)             # disable warnings
+
+
+if __name__ == '__main__':
+    dac = MCP4922()
+    print("Regular setVoltage() Function")
+    select_value = int(input("Select Value: "))
+    select_channel = int(input("Select Channel, 0 or 1: "))
+    try:
+        while True:
+            # Setting the voltage set point value
+            dac.setVoltage(select_channel, select_value)
+
+            # Read the Actual value from the sensor
+            print('Raw ADC Value: ', channel.value)
+            print('ADC Voltage: ' + str(channel.voltage) + 'V')
+            if channel.voltage > 2.0:
+                GPIO.output(17, True)
+            else:
+                GPIO.output(17, False)
+            time.sleep(0.5)
+            
+    except KeyboardInterrupt:   # Press CTRL C to exit program
+        dac.setVoltage(0, 0)
+        dac.setVoltage(1, 0)
+        dac.shutdown(0)
+        dac.shutdown(1)
+        GPIO.cleanup()
+        sys.exit(0)
+
+
+
+ 
+
+
+
+
